494fb4b594534f72d7521a8a871f0544bb02c16d
Zioraan <167449198+Zioraan@users.noreply.github.com>
2024-07-05 12:26:27 +0000

    Finished styling

--- src/js/component/timer.jsx
+++ src/js/component/timer.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 export default function Timer(props) {
     return (
-        <div className="bigCounter">
-            <div className="calendar">
-                <i className="far fa-clock"></i>
+        <div className="bigCounter d-flex w-50 h-50 container bg-black justify-content-around mt-3 p-1">
+            <div className='bg-secondary d-flex justify-content-around container'>
+                <div className="calendar bg-secondary">
+                    <i className="far fa-clock"></i>
+                </div>
+                <div className="four bg-secondary">{props.digitFour % 10}</div>
+                <div className="three bg-secondary">{props.digitThree % 10}</div>
+                <div className="two bg-secondary">{props.digitTwo % 10}</div>
+                <div className="one bg-secondary">{props.digitOne % 10}</div>
             </div>
-            <div className="four">{props.digitFour % 10}</div>
-            <div className="three">{props.digitThree % 10}</div>
-            <div className="two">{props.digitTwo % 10}</div>
-            <div className="one">{props.digitOne % 10}</div>
         </div>
     )
 }
